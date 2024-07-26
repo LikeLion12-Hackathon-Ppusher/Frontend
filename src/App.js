b99a6069cb92a1c756d68024be89210bf72953a8
변현석 <bhs3066@gmail.com>
2024-07-26 20:22:28 +0900

    feat : 카카오 로그인 페이지

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppDom>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />{" "}
+          {/* 초기 경로를 /main으로 리다이렉트 */}
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/home"></Route>
+        </Routes>
+      </AppDom>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+const AppDom = styled.div`
+  width: 600px;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/pages/HomeNav.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import LogoImgPng from "../logo.png";
+
+const HomeNav = () => {
+  const navigate = useNavigate();
+
+  // 로그인 화면으로 이동
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const REST_API_KEY = "ea433a9cc57e376f3ef82abbd8076b67";
+  const REDIRECT_URI = "http://localhost:3000/oauth";
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const loginHandler = () => {
+    window.location.href = link;
+  };
+
+  return (
+    <Container>
+      <LoginContainer>
+        <LoginBtn onClick={loginHandler}>로그인</LoginBtn>
+      </LoginContainer>
+      <HomeContainer>
+        <LogoContainer>
+          <LogoImg src={LogoImgPng} alt="LogoImg"></LogoImg>
+          Well-Scription
+          <EmptyContainer></EmptyContainer>
+        </LogoContainer>
+        <Navbar></Navbar>
+      </HomeContainer>
+    </Container>
+  );
+};
+
+export default HomeNav;
+
+const Container = styled.div``;
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`;
+
+const LoginBtn = styled.div`
+  cursor: pointer;
+`;
+
+const LogoContainer = styled.div`
+  height: 8.5rem;
+  width: 100%;
+  margin-top: 0.3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  background-color: #02d26b;
+`;
+
+const LogoImg = styled.img`
+  object-fit: contain;
+  margin-left: 20px;
+  heigth: 100px;
+  width: 100px;
+`;
+
+// 이걸로 space-between 사용해서 텍스틑 중앙정렬
+const EmptyContainer = styled.div`
+  width: 100px;
+`;
+
+const Navbar = styled.nav`
+  margin-top: 0.5rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  background-color: #02d26b;
+  padding: 0.8rem 0;
+`;
+
+const NavItem = styled.nav`
+  width: 100%;
+  cursor: pointer;
+`;
