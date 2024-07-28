2686f024de8fa4ca49163470df032d5f69379955
dev-joeysos <joeysos@naver.com>
2024-07-28 18:54:51 +0900

    style: 마이페이지

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -9,30 +9,40 @@ const Mypage = () => {
   };
 
   return (
-    <Container>
-      <BtnContainer>
-        마이페이지
+    <MyPageContainer>
+      <MyPageBtnContainer>
+        <MyPageHeader>마이페이지</MyPageHeader>
         <Btn onClick={handleSample}>카카오 계정 관리</Btn>
         <Btn onClick={handleSample}>사용자 유형 변경</Btn>
         <Btn onClick={handleSample}>알림 설정</Btn>
         <Btn onClick={handleSample}>내 제보 내역</Btn>
-      </BtnContainer>
-    </Container>
+      </MyPageBtnContainer>
+    </MyPageContainer>
   );
 };
 
 export default Mypage;
 
-const Container = styled.div`
+const MyPageContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100%;
+  background-color: white;
+  padding-bottom: 6rem;
 `;
 
-const BtnContainer = styled.div`
+const MyPageHeader = styled.h1`
+  display: flex;
+  justify-content: start;
+  position: absolute;
+  top: 6rem;
+  left: 2rem;
+`;
+
+const MyPageBtnContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -40,10 +50,11 @@ const BtnContainer = styled.div`
 `;
 
 const Btn = styled.button`
-  padding: 1rem 0rem 1rem 0rem;
-  margin-top: 0.5rem;
+  padding: 1.2rem 0rem 1.2rem 0rem;
+  margin-top: 1.5rem;
   width: 100%;
   border-radius: 6px;
   border: 1px solid black;
   background-color: white;
+  font-size: 18px;
 `;
