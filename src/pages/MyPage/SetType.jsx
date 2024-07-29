82d5f6c3efde0ec9ea92545b42a6587d159b55db
dev-joeysos <joeysos@naver.com>
2024-07-29 14:44:13 +0900

    fix: 유형변경 모달창 제거

--- src/pages/MyPage/SetType.jsx
+++ src/pages/MyPage/SetType.jsx
@@ -6,7 +6,6 @@ import backButtonImg from "../../assets/arrow-back.png";
 
 const SetType = () => {
   const [activeBox, setActiveBox] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleBoxClick = (box) => {
@@ -15,25 +14,11 @@ const SetType = () => {
 
   const handleConfirmClick = () => {
     if (activeBox) {
-      navigate("/home/set-type-check")
-      // setIsModalOpen(true);
+      localStorage.setItem('userType', activeBox);
+      navigate("/home/set-type-check");
     } else {
       alert("유형을 선택해 주세요.");
     }
-  };
-
-  const confirmChange = () => {
-    setIsModalOpen(false);
-
-    if (activeBox === "smoker") {
-      navigate("/select-smoker");
-    } else if (activeBox === "nonSmoker") {
-      navigate("/select-non-smoker");
-    }
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
   };
 
   return (
@@ -67,18 +52,6 @@ const SetType = () => {
         </Box>
       </SmokeSelect>
       <SelectBtn onClick={handleConfirmClick}>유형 변경하기</SelectBtn>
-
-      {isModalOpen && (
-        <ModalOverlay>
-          <Modal>
-            <h2>정말 변경하시겠습니까?</h2>
-            <ButtonContainer>
-              <ModalButton onClick={closeModal}>취소</ModalButton>
-              <ModalButton onClick={confirmChange}>확인</ModalButton>
-            </ButtonContainer>
-          </Modal>
-        </ModalOverlay>
-      )}
     </SelectContainer>
   );
 };
@@ -171,45 +144,4 @@ const SelectBtn = styled.div`
   text-align: center;
   cursor: pointer;
   font-size: 24px;
-`;
-
-const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-`;
-
-const Modal = styled.div`
-  background: white;
-  padding: 4rem 8rem 4rem 8rem;
-  border-radius: 8px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  text-align: center;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-  margin-top: 1rem;
-`;
-
-const ModalButton = styled.button`
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  padding: 0.5rem 1rem;
-  cursor: pointer;
-  font-size: 16px;
-
-  &:hover {
-    background-color: #0056b3;
-  }
 `;
