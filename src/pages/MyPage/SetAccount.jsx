80ddb69250432ffd4fab7a3099f2518baca569c1
dev-joeysos <joeysos@naver.com>
2024-07-29 20:05:33 +0900

    feat: 사용자 정보 불러오기

--- src/pages/MyPage/SetAccount.jsx
+++ src/pages/MyPage/SetAccount.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const SetAccount = () => {
   const navigate = useNavigate();
+  const [accessToken, setAccessToken] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('kakaoAccessToken');
+    setAccessToken(token);
+  }, []);
 
   const handleBack = () => {
     navigate("/home/mypage");
   };
 
+  const handleUserInfo = async () => {
+    const response = await axios.get('https://kapi.kakao.com/v2/user/me', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    }).then(response => {
+      console.log(response.data);
+      setUserInfo(response.data);
+    })
+  };
+
   return (
     <AccountContainer>
       계정 관리 탭
-      <BackBtn onClick={handleBack}>뒤로가기</BackBtn>
+      <BackBtn onClick={handleUserInfo}>사용자 정보 불러오기</BackBtn>
+      {userInfo && (
+        <UserInfo>
+          <p>이름: {userInfo.kakao_account.profile.nickname}</p>
+          <p>이메일: {userInfo.kakao_account.email}</p>
+          <ProfileImage src={userInfo.kakao_account.profile.profile_image_url} alt="Profile" />
+          <ThumbnailImage src={userInfo.kakao_account.profile.thumbnail_image_url} alt="Thumbnail" />
+        </UserInfo>
+      )}
     </AccountContainer>
   );
 };
@@ -30,4 +57,34 @@ const AccountContainer = styled.div`
 `;
 
 const BackBtn = styled.button`
+`;
+
+const UserInfo = styled.div`
+  margin-top: 20px;
+  text-align: center;
+
+  p {
+    margin: 5px 0;
+  }
+
+  img {
+    border-radius: 50%;
+    margin-top: 10px;
+  }
+`;
+
+const ProfileImage = styled.img`
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  margin-top: 10px;
+`;
+
+const ThumbnailImage = styled.img`
+  border-radius: 10%;
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  margin-top: 10px;
 `;
