90e8546599bc4ee09fb79f0649a937e388e3affe
dev-joeysos <joeysos@naver.com>
2024-08-03 04:59:08 +0900

    feat: 마이페이지 API 연동

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,14 +1,10 @@
 // src/services/api.js
-
 import axios from "axios";
-
 // 기본 설정
 const instance = axios.create({
   baseURL: "https://bbuhackathon.p-e.kr",
 });
-
 const baseURL = "https://bbuhackathon.p-e.kr";
-
 // POST 로그아웃
 export const logOut = async (token) => {
   try {
@@ -27,7 +23,6 @@ export const logOut = async (token) => {
     throw error;
   }
 };
-
 // GET 흡연 구역 안내
 export const getPlaceSmokingAPI = async (token) => {
   try {
@@ -37,17 +32,14 @@ export const getPlaceSmokingAPI = async (token) => {
       },
     });
     console.log("흡연 구역 안내:", response.data);
-
     const placeId = response.data.placeId;
     console.log("placeId:", placeId);
-
     return response.data;
   } catch (error) {
     console.error("GET 에러(place/smoking):", error);
     throw error;
   }
 };
-
 // GET 상세 흡연 구역 안내
 export const getPlaceSmokingIDAPI = async (token, id) => {
   try {
@@ -63,7 +55,6 @@ export const getPlaceSmokingIDAPI = async (token, id) => {
     throw error;
   }
 };
-
 // GET 사용자 정보
 export const getMyPageAPI = async (token) => {
   try {
@@ -78,7 +69,6 @@ export const getMyPageAPI = async (token) => {
     throw error;
   }
 };
-
 // PUT 사용자 유형
 export const putUserTypeAPI = async (token, type) => {
   try {
@@ -99,7 +89,6 @@ export const putUserTypeAPI = async (token, type) => {
     throw error;
   }
 };
-
 // 흡연자의 흡연구역 제보
 export const smokerReportAPI = async (
   access_Token,
@@ -120,18 +109,19 @@ export const smokerReportAPI = async (
     console.log(Lng);
     console.log(address);
     console.log(title);
+    console.log(indoorOutdoor);
     const response = await axios.post(
       `${baseURL}/report/`,
       {
         reportType: reportUserType,
         userId: userId,
-        smokingPlace: {
+        reportSmokingPlace: {
           latitude: Lat,
           longitude: Lng,
-          name: address,
+          name: title,
           address: address,
-          type: indoorOutdoor,
-          ashtray: hasAshtray,
+          isIndoor: true, // 임시
+          ashtray: true, // 임시
           rate: cleanlinessRating,
         },
         description: title,
@@ -142,13 +132,13 @@ export const smokerReportAPI = async (
         },
       }
     );
+    console.log(response);
     return response.data;
   } catch (error) {
     console.error("흡연자 제보 에러", error);
     throw error;
   }
 };
-
 // 비흡연자의 흡연구역 제보
 export const nonSmokerReportAPI = async (
   access_Token,
@@ -174,7 +164,7 @@ export const nonSmokerReportAPI = async (
         secondhandSmokingPlace: {
           latitude: Lat,
           longitude: Lng,
-          name: address,
+          name: title,
           address: address,
         },
         description: title,
@@ -191,7 +181,6 @@ export const nonSmokerReportAPI = async (
     throw error;
   }
 };
-
 // PUT 알림설정
 export const putAlarmOptionAPI = async (token, opt) => {
   try {
@@ -211,6 +200,40 @@ export const putAlarmOptionAPI = async (token, opt) => {
   } catch (error) {
     console.error("PUT 에러(alarm)", error);
     throw error;
+  }
+};
+
+// GET 나의 제보 내역
+export const getMyPageReportAPI = async () => {
+  try {
+    const token = localStorage.getItem('access_token');
+    const response = await axios.get(`${baseURL}/oauth/user/mypage/report/`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    console.log('나의 제보 내역:', response.data);
+    return response.data;  
+  } catch (error) {
+    console.error('GET 에러(mypage/report):', error);
+    throw error;  
+  }
+};
+
+// GET 나의 제보 상세 내역
+export const getReportDetailAPI = async (reportId) => {
+  try {
+    const token = localStorage.getItem('access_token');
+    const response = await axios.get(`${baseURL}/oauth/user/mypage/report/${reportId}/`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    console.log('나의 제보 상세 내역:', response.data);
+    return response.data;  
+  } catch (error) {
+    console.error('GET 에러(mypage):', error);
+    throw error;  
   }
 };
 
